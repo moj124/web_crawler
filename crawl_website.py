6169c6f59cbc74f82f59a0110f3869c300d8a0d0
michael jones <michael.jowen.jones@gmail.com>
2021-10-22 11:01:29 +0100

    reformated get_links and improved error handling

--- crawl_website.py
+++ crawl_website.py
@@ -63,102 +63,88 @@ class Crawler():
 
     # check if queue is not empty
     if not self.queue:
-      return None
+      raise ValueError("Queue is empty")
+    
+    return self.queue.pop()
 
-    # select one link from the queue of links
-    selected_index = random.choice(range(len(self.queue)))
-    selected_link = self.queue[selected_index]
+    # # select one link from the queue of links
+    # selected_index = random.choice(range(len(self.queue)))
+    # selected_link = self.queue[selected_index]
 
-    # delete the selected link from the queue of links
-    del self.queue[selected_index]
-    return selected_link
+    # # delete the selected link from the queue of links
+    # del self.queue[selected_index]
+    # return selected_link
 
   def get_links(self):
     """ Scrapes the selected link from get_next_link webpage for other local links"""
-    try:
-      # Get a random link from the queue
+    while self.queue:
       try:
-        url = self.get_next_link()
-      except None as e:
-        print("no link generated")  
-      self.visited.add(url)
-      
-      # Document parent to child link relation
-      self.output[url] = []
-      print("Processing %s" % url)
+        # Get a random link from the queue
+        url = self.get_next_link() 
+        self.visited.add(url)
+        
+        # Document parent to child link relation
+        self.output[url] = []
+        print("Processing %s" % url)
 
-      try:
-        requests.head(url)
-      except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL, requests.exceptions.InvalidSchema):
-        # add broken urls to it's own set, then continue
-        self.broken_links.add(url)
-        print("request failed")    
-
-      try:
+        requests.head(url)   
         req = requests.get(url)
-      except:
-        # add unauthorized request from website response to it's own set, then continue
-        self.broken_links.add(url)
-        print("unauthorised get request")  
-  
-      try:
         soup = BeautifulSoup(req.content, 'html.parser')
-      except:
+
+
+        # extract sections of url to resolve relative links
+        domain_name, webpage, path = get_parts_of_url(url)
+
+        for link in soup.find_all('a'): 
+          # extract link url from the anchor from <a> tag 
+          anchor = link.attrs["href"] if "href" in link.attrs else ''
+        
+          # capture all links that have partially relative links, 
+          # full links or links with parts and domain name
+          # add all to local_links set
+          if anchor.startswith('/'):
+            # anchor contains a partial url link relative to the url      
+            local_link = webpage + anchor
+
+            # record the link url relation
+            self.output[url].append(local_link)         
+            self.local_links.add(local_link)    
+          elif domain_name in anchor:   
+            # record the link url relation
+            self.output[url].append(anchor)
+            self.local_links.add(anchor)    
+          elif not anchor.startswith('http'):  
+            # anchor contains a partial url link relative to the url or domain and part    
+            local_link = path + anchor      
+
+            # record the link url relation
+            self.output[url].append(local_link) 
+            self.local_links.add(local_link)    
+        
+        # add all unvisited and not in the queue links from the local_links
+        for i in self.local_links:    
+          if not i in self.queue and not i in self.visited:          
+            self.queue.append(i)
+            self.output[url] = list(set(self.output[url]))
+
+      except Exception as e:
+        # unknown error has occured
         self.broken_links.add(url)
-        print("failed to read body")  
+        print(e)
 
-      # extract sections of url to resolve relative links
-      domain_name, webpage, path = get_parts_of_url(url)
-
-      for link in soup.find_all('a'): 
-        # extract link url from the anchor from <a> tag 
-        anchor = link.attrs["href"] if "href" in link.attrs else ''
-      
-        # capture all links that have partially relative links, 
-        # full links or links with parts and domain name
-        # add all to local_links set
-        if anchor.startswith('/'):
-          # anchor contains a partial url link relative to the url      
-          local_link = webpage + anchor
-
-          # record the link url relation
-          self.output[url].append(local_link)         
-          self.local_links.add(local_link)    
-        elif domain_name in anchor:   
-          # record the link url relation
-          self.output[url].append(anchor)
-          self.local_links.add(anchor)    
-        elif not anchor.startswith('http'):  
-          # anchor contains a partial url link relative to the url or domain and part    
-          local_link = path + anchor      
-
-          # record the link url relation
-          self.output[url].append(local_link) 
-          self.local_links.add(local_link)    
-      
-      # add all unvisited and not in the queue links from the local_links
-      for i in self.local_links:    
-        if not i in self.queue and not i in self.visited:          
-          self.queue.append(i)
-          self.output[url] = list(set(self.output[url]))
-
-    except Exception as e:
-      # unknown error has occured
-      print(e)
-
-  def run(self,maximum_threads= 1000,maximum_timeout=5):
+  def run(self,maximum_workers,maximum_timeout=5):
     """Runs get_links() asynchronously until all links from the queue are visiteda nd raises errors in case of invalid parameters
 
     Parameters
     ----------
-    maximum_threads : int
-        Specifies the maxmium number of threads that can be spawned
+    maximum_workers : int
+        Specifies the maxmium number of workers that can be spawned
     maximum_timeout : int
         Specifies the maxmium alloted time for a gevent thread to process a job
     """
 
-    if maximum_threads <= 0: 
-      myError = ValueError('maximum_threads should be a natural number')
+    if maximum_workers <= 0: 
+      myError = ValueError('maximum_workers should be a natural number')
       raise myError
     elif maximum_timeout <= 0:
       myError = ValueError('maximum_timeout should be a natural number')
@@ -167,13 +153,10 @@ class Crawler():
     # record the time performance of the program
     start_time = time.time()
 
-    # continuously spawn threads of get_links 
-    # until there are no more links to visit
-    while self.queue:
-      # spawn threads according to the queue length and defined max thread count
-      threads = [spawn(self.get_links) for i in range(maximum_threads) if i+1 <= len(self.queue)]
-      # await until threads finish execution
-      joinall(threads,timeout=maximum_timeout)
+    # spawn workers according to the queue length and defined max thread count
+    workers = [spawn(self.get_links) for i in range(maximum_workers)]
+    # await until workers finish execution
+    joinall(workers)
     print()
     print("--- %s seconds ---" % (time.time() - start_time))  
 
@@ -202,20 +185,20 @@ def main():
     # the list of arguements avaliable
     parser.add_argument('--webpage','-l', type=str,
                         default='https://www.bbc.co.uk/', help='the starting webpage that the crawler will extract urls from')
-    parser.add_argument('--maximum_threads','-n', type=int, default=1000,
-                        help='number of threads being used asynchronously at one time')
+    parser.add_argument('--maximum_workers','-n', type=int, default=1000,
+                        help='number of workers being used asynchronously at one time')
     parser.add_argument('--savetofile','-s', type=bool, default=False,
                         help='save local link relations from output to json file')
     parser.add_argument('--data_directory','-d', type=str, default='data/',
                         help='directory to save the output file to')
     parser.add_argument('--maximum_processing_time','-t', type=int, default=10,
-                        help='gevent thread timeout parameter in seconds before threads is killed')
+                        help='gevent thread timeout parameter in seconds before workers is killed')
 
     # parse the terminal arguements for custom execution
     opt = parser.parse_args()
     # run the program with the given arguements
     web_crawler = Crawler(opt.webpage)
-    web_crawler.run(opt.maximum_threads,opt.maximum_processing_time)
+    web_crawler.run(opt.maximum_workers,opt.maximum_processing_time)
 
     # display results
     print("%d local links were found" % len(web_crawler.local_links))
